2c3e19fd028c80bdfd0efe4f39962b048e75d1ef
confused_techie <dev@lhbasics.com>
2020-11-12 20:36:42 -0800

    Working on Better Bookmark layout

---
+++ Tabby_Docker/Pages/Testing.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tabby_Docker.Pages
+{
+    public class TestingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
